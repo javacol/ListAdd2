920c26d57f980f180be98aabb6430c8db39d60e7
pc <pc@PC001>
2018-05-21 12:19:30 +0900

    update

--- src/application/ListAddController.java
+++ src/application/ListAddController.java
@@ -1,15 +1,14 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -20,24 +19,25 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.stage.FileChooser;
 
-public class ListAddController implements Initializable{
+public class ListAddController implements Initializable {
 
 	@FXML
 	private ListView<String> listview;
 	@FXML
 	private TextField txt;
 	@FXML
-	private Button btn_add,btn_delete,btn_alldelete,btn_save,btn_open;
+	private Button btn_add, btn_delete, btn_alldelete, btn_save, btn_open;
 	@FXML
 	private Label lbl;
 
 	private ObservableList<String> items;
-
 	private StringBuilder builder = new StringBuilder();
-
 	final FileChooser fc = new FileChooser();
 
 	@Override
@@ -48,7 +48,10 @@ public class ListAddController implements Initializable{
 		listview.setCellFactory(TextFieldListCell.forListView());
 
 		fc.setTitle("ファイル選択");
-
+		fc.getExtensionFilters().addAll(
+				new FileChooser.ExtensionFilter("CSV", "*.csv"),
+				new FileChooser.ExtensionFilter("All", "*.*"),
+				new FileChooser.ExtensionFilter("text", "*.txt"));
 	}
 
 	@FXML
@@ -56,7 +59,6 @@ public class ListAddController implements Initializable{
 		String str = txt.getText();
 
 		items.add(str);
-
 		items.stream().forEach((s) -> {
 			System.out.println(s);
 		});
@@ -65,18 +67,22 @@ public class ListAddController implements Initializable{
 	@FXML
 	private void btn_delete_onClick(MouseEvent e) {
 		final int selectedIdx = listview.getSelectionModel().getSelectedIndex();
-		if(selectedIdx != -1) {
-			String itemToRemove = listview.getSelectionModel().getSelectedItem();
-
-			final int newSelectedIdx = (selectedIdx == listview.getItems().size() - 1)
-					? selectedIdx - 1 : selectedIdx;
-
-			listview.getItems().remove(selectedIdx);
-			listview.getSelectionModel().select(newSelectedIdx);
-			System.out.println("selectIdx:" + selectedIdx);
-			System.out.println("item:" + itemToRemove);
-			//items.remove(selectedIdx);
+		if (selectedIdx != -1) {
+			delete(selectedIdx);
 		}
+	}
+
+	private void delete(final int selectedIdx) {
+		String itemToRemove = listview.getSelectionModel().getSelectedItem();
+
+		final int newSelectedIdx = (selectedIdx == listview.getItems().size() - 1)
+				? selectedIdx - 1
+				: selectedIdx;
+
+		listview.getItems().remove(selectedIdx);
+		listview.getSelectionModel().select(newSelectedIdx);
+		System.out.println("selectIdx:" + selectedIdx);
+		System.out.println("item:" + itemToRemove);
 	}
 
 	@FXML
@@ -86,42 +92,47 @@ public class ListAddController implements Initializable{
 
 	@FXML
 	private void btn_save_onClick(MouseEvent e) {
-//		for(String str : items) {
-//			builder.append(str).append(",");
-//		}
-//
-//		String result = builder.toString();
-
 		File saveFile = fc.showSaveDialog(null);
 
-		if(saveFile != null) {
-			lbl.setText(saveFile.getPath().toString());
+		if (saveFile != null) {
+			fileSave(saveFile,items);
 		}
+	}
 
-		fc.setInitialDirectory(new File("c:/2018_2nen/"));
-		fc.setInitialFileName("save.txt");
-
+	private void fileSave(File saveFile , ObservableList<String> items) {
+		lbl.setText(saveFile.getPath().toString());
+		try {
+			FileWriter fw = new FileWriter(saveFile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			for (String output : items) {
+				bw.write(output);
+				bw.write(",");
+			}
+			bw.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@FXML
 	private void btn_open_onClick(MouseEvent e) {
-		FileFilter filter = new FileNameExtensionFilter("text","txt");
-		fc.addChoosableFileFilter(filter);
 		File openFile = fc.showOpenDialog(null);
 
-		if(openFile != null) {
-			lbl.setText(openFile.getPath().toString());
+		if (openFile != null) {
+			fileOpen(openFile, items);
 		}
+	}
 
+	private void fileOpen(File openFile, ObservableList<String> items) {
+		lbl.setText(openFile.getPath().toString());
 		try {
-			BufferedReader br =new BufferedReader(new FileReader(openFile));
-
+			BufferedReader br = new BufferedReader(new FileReader(openFile));
 			String line;
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				items.addAll(line.split(",", 0));
-//				for(String elem : list) {
-//					items.add(elem);
-//				}
+				//for(String elem : list) {
+				//items.add(elem);
+				//}
 			}
 			br.close();
 		} catch (FileNotFoundException e1) {
@@ -131,5 +142,11 @@ public class ListAddController implements Initializable{
 		}
 	}
 
-
+	private void setOnDragDetected(MouseEvent e) {
+		Dragboard db = listview.startDragAndDrop(TransferMode.ANY);
+		System.out.println("DragDetected");
+		ClipboardContent content = new ClipboardContent();
+		content.putString(listview.getSelectedText());
+		db.setContent(content);
+	}
 }
